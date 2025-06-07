4386e53fea6d2cf8a9f34c5ba3b8351073f6d07a
Deepak Kumar Saxena <deepzsenu@gmail.com>
2025-06-07 17:45:04 +0530

    added leetcode and solved basic python problems

---
+++ Python/2.FunctionsInPython/YeildFunction/Yeild.py
@@ -0,0 +1,12 @@
+'''
+Yield is a keyword in Python that is used to return from a function without 
+destroying the states of its local variable and when the function is called, 
+the execution starts from the last yield statement. Any function that contains 
+a yield keyword is termed a generator. Hence, yield is what makes a generator. 
+The yield keyword in Python is less known off but has a greater utility which one 
+can think of.
+
+
+Code #1 : Demonstrating yield working 
+'''
+
